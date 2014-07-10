42f079de4b59c18de82baa8ca414e0a7e5cd4376
Pisces000221 <1786762946@qq.com>
2014-07-10 18:59:02 +0800

    Import colour resources from 2048, show score and best score

--- src/scenes/gameplay.lua
+++ src/scenes/gameplay.lua
@@ -10,11 +10,39 @@ function app.scenes.gameplay:create()
     scene:addChild(bg, -1)
 
     -- the big '2048' in the top-left corner
-    local lbl_title = app.label(' 2048', 72, true)
-    lbl_title:setColor(app.res.colours.front._3b)
-    lbl_title:setAnchorPoint(cc.p(0, 1))
-    lbl_title:setPosition(display.normalize(0, 1))
-    scene:addChild(lbl_title)
+    local max_diametre = display.size.height - display.size.width
+    local bbl_title = app.widgets.bubble:create(max_diametre - 12, app.res.colours.tile[2048][1])
+    bbl_title:setAnchorPoint(cc.p(0, 1))
+    bbl_title:setPosition(cc.p(6, display.size.height - 6))
+    scene:addChild(bbl_title, 1)
+    local lbl_title = app.label('2048', 72, true)
+    lbl_title:setColor(app.res.colours.tile[2048][2])
+    lbl_title:setPosition(cc.p(max_diametre * 0.5, display.size.height - max_diametre * 0.5))
+    scene:addChild(lbl_title, 2)
+
+    -- the score board
+    -- current score
+    local lbl_score = app.label('SCORE', 30, false)
+    lbl_score:setColor(app.res.colours.front._3b)
+    lbl_score:setPosition(cc.p(
+      (display.size.width + max_diametre) * 0.5, display.size.height - max_diametre * 0.2))
+    scene:addChild(lbl_score)
+    local score_disp = app.label('0', 45, true)
+    score_disp:setColor(app.res.colours.front._3b)
+    score_disp:setPosition(cc.p(
+      (display.size.width + max_diametre) * 0.5, display.size.height - max_diametre * 0.4))
+    scene:addChild(score_disp)
+    -- high score
+    local lbl_hiscore = app.label('BEST', 30, false)
+    lbl_hiscore:setColor(app.res.colours.front._3b)
+    lbl_hiscore:setPosition(cc.p(
+      (display.size.width + max_diametre) * 0.5, display.size.height - max_diametre * 0.6))
+    scene:addChild(lbl_hiscore)
+    local hiscore_disp = app.label('0', 45, true)
+    hiscore_disp:setColor(app.res.colours.front._3b)
+    hiscore_disp:setPosition(cc.p(
+      (display.size.width + max_diametre) * 0.5, display.size.height - max_diametre * 0.8))
+    scene:addChild(hiscore_disp)
 
     return scene
 end
--- src/widgets/bubble.lua
+++ src/widgets/bubble.lua
@@ -3,7 +3,11 @@ app.widgets.bubble = app.widgets.bubble or {}
 
 -- create(number diametre, cc.c3b colour)
 function app.widgets.bubble:create(diametre, colour)
-    local sprite = cc.Sprite:create('res/images/circle.png')
+    local sprite
+    if diametre >= 144 then sprite = cc.Sprite:create('res/images/circle_3x.png')
+    elseif diametre >= 96 then sprite = cc.Sprite:create('res/images/circle_2x.png')
+    elseif diametre >= 64 then sprite = cc.Sprite:create('res/images/circle_1.5x.png')
+    else sprite = cc.Sprite:create('res/images/circle.png') end
     sprite:setScale(diametre / sprite:getContentSize().width)
     sprite:setColor(colour)
     return sprite
