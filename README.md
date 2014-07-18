ff98f434038f05dd05738495bccbf3f8d29c1b2b
Pisces000221 <1786762946@qq.com>
2014-07-18 22:07:59 +0800

    Add GPL v2 declaration

--- README.md
+++ README.md
@@ -16,7 +16,9 @@ Then you're ready to build &amp; run!
 首先，请配置好Cocos2d-x 3.2rc0的开发环境，新建一个工程，把这个项目的文件复制进去替换掉就可以开始编译了。
 
 [brogrammer] Which licence do you use for this project?<br>
-We use GPL v2 for the code. There're no artworks, so don't worry about that. The font follows the SIL.
+We use GPL v2 for the code. See the file LICENSE for details.<br>
+There're no artworks, so don't worry about that. The font follows the SIL.
 
 [猿] 这个项目源码的协议是什么？<br>
-我们的源码使用的是GPL v2。没有图片/音乐素材，所以不必担心。使用的字体遵循SIL协议。
+我们的源码使用的是GPL v2（详见LICENSE文件）。<br>
+这里没有图片/音乐素材，所以不必担心。使用的字体遵循SIL协议。
--- src/rock.lua
+++ src/rock.lua
@@ -50,3 +50,22 @@ end
 
 local status, msg = xpcall(main, __G__TRACKBACK__)
 if not status then error(msg) end
+
+--[[
+    Soda2048
+    Copyright (C) 2014  Pisces000221 (Shiqing Lyu)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+]]
